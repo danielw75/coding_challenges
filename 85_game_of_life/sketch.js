508ff06688f5267f5904e94f0c632f8cfad23b05
danielw75 <63822535+danielw75@users.noreply.github.com>
2020-09-05 11:14:24 -0300

    Build structure with numbers

--- 85_game_of_life/sketch.js
+++ 85_game_of_life/sketch.js
@@ -1,10 +1,26 @@
 function make2DArray(cols, rows) {
-  
+
+  //	Build grid structure w/ even columns and rows
+  let arr = new Array(cols);
+
+  for (let i = 0; i < arr.length; i++) {
+  	arr[i] = new Array(rows);
+  }
+  return arr;
 }
-//	Variable grid
+
+//	Variables of grid
 let grid;
 
 function setup() {
-	//	Make a grid
+
+	//	Define Grid 2D Array
 	grid = make2DArray(10, 10);
+
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			//	Fill it w/ 0s and 1s
+			grid[i][j] = floor(random(2));
+		}
+	}
 }
