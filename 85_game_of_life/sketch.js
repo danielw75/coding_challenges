3f9079f51fbf7a182bfd801bb4e5ec3bf3353c6f
danielw75 <63822535+danielw75@users.noreply.github.com>
2020-09-05 15:11:05 -0300

    Complete program

--- 85_game_of_life/sketch.js
+++ 85_game_of_life/sketch.js
@@ -1,10 +1,7 @@
 function make2DArray(cols, rows) {
-
-  //	Build grid structure w/ even columns and rows
   let arr = new Array(cols);
-
   for (let i = 0; i < arr.length; i++) {
-  	arr[i] = new Array(rows);
+    arr[i] = new Array(rows);
   }
   return arr;
 }
@@ -12,40 +9,69 @@ function make2DArray(cols, rows) {
 let grid;
 let cols;
 let rows;
-// Set resolution for cell size
-let resolution = 40;
+let resolution = 5;
 
 function setup() {
-	createCanvas(800, 800);
+  frameRate(5);
+  createCanvas(800, 800);
+  cols = width / resolution;
+  rows = height / resolution;
 
-	cols = width/resolution;
-	rows = height/resolution;
-
-	//	2D Array
-	grid = make2DArray(cols, rows);
-
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			
-			grid[i][j] = floor(random(2));
-		}
-	}
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
 }
 
 function draw() {
-	background(0);
+  background(0);
 
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			
-			let x = i * resolution;
-			let y = j * resolution;	
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
 
-			if (grid[i][j] == 1) {
-				fill(255);
-				stroke(0);
-				rect(x, y, resolution - 1, resolution - 1);
-			}
-		}
-	}
+  let next = make2DArray(cols, rows);
+
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+
+  grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
 }
